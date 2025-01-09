1122921b074f406097bfce69fe07ac797b6eecea
Kasia Wronska <wronska.katarzyna97@gmail.com>
2025-01-09 18:27:10 +0100

    Exceptions in bookings options. New request function (downloading data for one booking).

--- backend/bookings.py
+++ backend/bookings.py
@@ -5,7 +5,7 @@ import requests
 
 HOTEL_API_URL = "https://api.content.tripadvisor.com/api/v1/location/nearby_search"
 
-with open('config.json', 'r+') as file:
+with open('./backend/config.json', 'r+') as file:
     config = json.load(file)
 
 
@@ -16,14 +16,71 @@ def get_accommodation_data(lat, lon):
         "category": "hotels"
     }
     response = requests.get(url=HOTEL_API_URL, params=data)
-    response.raise_for_status()
+    code = response.status_code
     resp = response.json()
-    for item in resp['data']:
-        print(item)
+
+    if code >= 400:  # code 4xx
+        output = json.loads(resp.content)
+        output['code'] = code
+        return output
+    output = resp
+    output['code'] = code
+    return output
+
+
+def get_single_location_data(location_id):
+    LOCATION_DETAILS_URL = f"https://api.content.tripadvisor.com/api/v1/location/{location_id}/details"
+    data = {
+        "key": config["TRIPADVISOR_API_KEY"],
+        "language": "en",
+        "currency": "USD"
+    }
+    headers = {"accept": "application/json"}
+
+    response = requests.get(url=LOCATION_DETAILS_URL, params=data, headers=headers)
+    resp = response.json()
+    if response.status_code >= 400:  # code 4xx
+        output = resp
+        output['code'] = response.status_code
+        return output
+    result = {}
+    result['code'] = response.status_code
+    result['location_id'] = resp['location_id']
+    result["name"] = resp["name"]
+    result["web_url"] = resp["web_url"]
+    result["address_obj"] = resp["address_obj"]
+    result["rating"] = resp["rating"] if "rating" in resp else "brak"
+    result["price_level"] = resp["price_level"] if "price_level" in resp else "brak"
+    # print(response.text)
+    return result
+
+def get_location_ids(bookings_data):
+    ids = []
+    for entry in bookings_data['data']:
+        ids.append(entry['location_id'])
+    return ids
+
+def get_all_location_data(bookings_data):
+    ids = get_location_ids(bookings_data)
+    all_location_data = []
+    for id in ids:
+        single_location_data = get_single_location_data(id)
+        all_location_data.append(single_location_data)
+    
+    return all_location_data
 
 
 if __name__ == '__main__':
     from weather import get_forecast
     data = get_forecast("Konin", datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(days=5))
+    pass
+    # print(data)
+    # booking_data = get_accommodation_data(data['lat'], data['lon'])
 
-    get_accommodation_data(data['lat'], data['lon'])
+    booking_data = get_accommodation_data("test", "test")
+    print(booking_data)
+
+    result = get_single_location_data(booking_data['data'][0]['location_id'])
+    print(result)
+    # result = get_single_location_data('dfvjyhsabfjsnm1234567')
+    # print(result)
